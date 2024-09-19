578113296ae8450306e7933bda0f97490e2e2ed6
ryan-albright <ryan.albright@colorado.edu>
2024-09-19 15:33:48 -0600

    v2

--- Labs/Lab-4/fixedpt.py
+++ Labs/Lab-4/fixedpt.py
@@ -4,32 +4,22 @@ import numpy as np
 def driver():
 
 # test functions 
-     f1 = lambda x: 1+0.5*np.sin(x)
-# fixed point is alpha1 = 1.4987....
+     f1 = lambda x: np.sqrt(10 / (x + 4))
 
-     f2 = lambda x: 3+2*np.sin(x)
-#fixed point is alpha2 = 3.09... 
+#fixed point  
 
-     Nmax = 100
-     tol = 1e-6
-
-# test f1 '''
-     x0 = 0.0
+     Nmax = 15
+     tol = 1e-10
+    
+#test f1 '''
+     x0 = 1.5
      [xstar, x_list, ier] = fixedpt(f1,x0,tol,Nmax)
+     [_lambda, alpha] = compute_order(x_list, xstar)
      print('the approximate fixed point is:',xstar)
      print('the list of iterates is:', x_list)
      print('f1(xstar):',f1(xstar))
      print('Error message reads:',ier)
-    
-#test f2 '''
-     x0 = 0.0
-     [xstar, x_list, ier] = fixedpt(f2,x0,tol,Nmax)
-     print('the approximate fixed point is:',xstar)
-     print('the list of iterates is:', x_list)
-     print('f2(xstar):',f2(xstar))
-     print('Error message reads:',ier)
-
-
+     print(f"The order of convergence is {np.round(alpha, 1)}")
 
 # define routines
 def fixedpt(f,x0,tol,Nmax):
@@ -54,5 +44,26 @@ def fixedpt(f,x0,tol,Nmax):
     ier = 1
     return [xstar, x_list, ier]
     
+def compute_order(x_list, xstar):
+    """
+    Approximates order of convergence given 
+    x_list: list of iterates
+    x_star: fixed point/solution
+    """
+    x_list = x_list[0:-1]
+    diff1 = np.abs(x_list[1::] - xstar)
+    print(diff1)
+    diff2 = np.abs(x_list[0:-1] - xstar)
+    print(diff2)
+    fit = np.polyfit(np.log(diff2.flatten()), np.log(diff1.flatten()), 1)
+
+    _lambda = np.exp(fit[1])
+    alpha = fit[0]
+    """
+    lamba  = e^intercept
+    alpha = slope
+    """
+
+    return [_lambda, alpha]
 
 driver()
